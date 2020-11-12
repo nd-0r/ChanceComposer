5184164aebca2b3aa1ec2b02cc1c6b039efe4b4f
Andrew Orals <nd-0r@andreworals.com>
2020-11-12 12:10:29 -0600

    refactored and added chord implementation

--- generator.py
+++ generator.py
@@ -9,6 +9,7 @@ from pathlib import Path
 
 MAX_MIDI = 84
 MIN_MIDI = 60
+MAX_CHORD_SIZE = 4
 
 instruments = [instrument.Flute, instrument.Clarinet, instrument.Violin, instrument.Violoncello, instrument.Piano]
 class range_enum(Enum):
@@ -29,8 +30,8 @@ random.shuffle(time_signatures)
 
 ### END CONSTANTS ###
 
-
-def measure_generator(time_sig, inst_range):
+# generate a random measure
+def measure_generator(time_sig, inst_range, chords=False):
     bar = stream.Measure()
     bar.timeSignature = meter.TimeSignature(time_sig)
     current_duration = 0
@@ -42,25 +43,55 @@ def measure_generator(time_sig, inst_range):
                 current_duration += to_add.duration._getQuarterLength()
                 bar.append(to_add)
         else:
-            to_add = note.Note(random.randint(max(MIN_MIDI, inst_range[0]), min(inst_range[1], MAX_MIDI)))
-            to_add.duration = durations[random.randint(0, len(durations) - 1)]
-            if (current_duration + to_add.duration._getQuarterLength() <= bar.timeSignature.barDuration._getQuarterLength()):
-                current_duration += to_add.duration._getQuarterLength()
-                bar.append(to_add)
+            if (chords == True):
+                chord_size = random.ranint(0, MAX_CHORD_SIZE)
+                pitches = [pitch.Pitch(random.randint(max(MIN_MIDI, inst_range[0]), min(inst_range[1], MAX_MIDI))) for i in range(0, chord_size)]
+                to_add = chord.Chord(pitches)
+                to_add.duration = durations[random.randint(0, len(durations) - 1)]
+                if (current_duration + to_add.duration._getQuarterLength() <= bar.timeSignature.barDuration._getQuarterLength()):
+                    current_duration += to_add.duration._getQuarterLength()
+                    bar.append(to_add)
+            else:
+                to_add = note.Note(random.randint(max(MIN_MIDI, inst_range[0]), min(inst_range[1], MAX_MIDI)))
+                to_add.duration = durations[random.randint(0, len(durations) - 1)]
+                if (current_duration + to_add.duration._getQuarterLength() <= bar.timeSignature.barDuration._getQuarterLength()):
+                    current_duration += to_add.duration._getQuarterLength()
+                    bar.append(to_add)
     return bar
-        
+
 # make the parts
-parts = [stream.Part() for i in range(0, len(instruments))]
-for i,part in enumerate(parts):
-    part.instrument = instruments[i]
+def part_generator(part, chords=False):
     for b in range(0, BARS):
-        part.append(measure_generator(time_signatures[b % len(time_signatures)], ranges[i].value))
+        part.append(measure_generator(time_signatures[b % len(time_signatures)], ranges[i].value, chords))
+        
+# make the score
+def score_generator():
+    parts = []
+    for i in range(0, len(instruments)):
+        if ('KeyboardInstrument' in instruments[i].classes):
+            upper = stream.Part()
+            lower = stream.Part()
+            upper.instrument = instruments[i]
+            part_generator(upper, True)
+            lower.instrument = instruments[i]
+            part_generator(lower, False)
+            parts.append(layout.StaffGroup([upper, lower], name=instruments[i].bestName(), abbreviation=instruments[i].instrumentAbbreviation, symbol='brace'))
+        else:
+            p = stream.Part()
+            p.instrument = instruments[i]
+            part_generator(p)
+            parts.append(layout.StaffGroup([p], name=instruments[i].bestName(), abbreviation=instruments[i].instrumentAbbreviation, symbol='bracket'))
+    return parts
 
 if __name__ == '__main__':
+    BARS = int(input("How many bars? ") or '8')
+    REST_DISTRIBUTION = float(input("Enter the distribution of rests as a decimal between 0 and 1 (default 0.2): ") or '0.2')
+    MAX_CHORD_SIZE = int(input("Enter the maximum chord size in notes (default 4): ") or '4')
     out = input("Enter output path (default is current directory): ")
 
-# make the score
-sc = stream.Score(parts)
+
+# drive the code
+sc = stream.Score(score_generator())
 if (out == ""):
     p = Path('./ChanceComposition')
 else:
